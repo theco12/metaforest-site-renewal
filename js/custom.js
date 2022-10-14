30f675b6e4fb427df6c6e57baa3621e686ae5b39
theco12 <107103821+theco12@users.noreply.github.com>
2022-10-14 18:21:39 +0900

    update section2

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".nav__menu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+// <!-- Initialize Swiper -->
+
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+});
