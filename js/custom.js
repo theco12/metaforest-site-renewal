5a1e14926fd5e6ded1c16c63a7ff24fff531511f
theco12 <107103821+theco12@users.noreply.github.com>
2022-10-24 18:48:21 +0900

    update

--- js/custom.js
+++ js/custom.js
@@ -32,3 +32,49 @@ var flkty = new Flickity(elem, {
   wrapAround: true,
   pageDots: false,
 });
+
+// certification slide
+var slides = document.querySelector(".slides"),
+  slide = document.querySelectorAll(".slides li"),
+  currentIdx = 0;
+(slideCount = slide.length),
+  (slidewidth = 400),
+  (slideMargin = 40),
+  (nextBtn = document.querySelector(".next_slide")),
+  (prevBtn = document.querySelector(".prev_slide"));
+
+slides.style.width =
+  (slidewidth + slideMargin) * slideCount - slideMargin + "px";
+
+function moveSlide(num) {
+  slides.style.left = -num * 440 + "px";
+  currentIdx = num;
+}
+
+nextBtn.addEventListener("click", function () {
+  if (currentIdx < slideCount - 8) {
+    moveSlide(currentIdx + 1);
+  } else {
+    moveSlide(0);
+  }
+});
+
+prevBtn.addEventListener("click", function () {
+  if (currentIdx > 0) {
+    moveSlide(currentIdx - 1);
+  } else {
+    moveSlide(slideCount - 10);
+  }
+});
+
+//인증서보기 버튼 클릭시 slidewrapper가 켜짐
+
+function cert_Popup() {
+  const popup_cert = document.getElementById("cert_slide_content");
+  popup_cert.style.display = "block";
+}
+
+function cert_close_Popup() {
+  const popup_cert = document.getElementById("cert_slide_content");
+  popup_cert.style.display = " none";
+}
